dd10a3c5d74b45b4e58fe8f5509fa7cd461d1785
RealSong88 <july2day@gmail.com>
2022-05-30 07:30:17 +0900

    first commit

---
+++ servlet/src/main/java/hello/servlet/basic/TestController.java
@@ -0,0 +1,16 @@
+package hello.servlet.basic;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	
+	public String test() {
+		Test test = new Test();
+		
+		
+		
+		return "";
+	}
+}

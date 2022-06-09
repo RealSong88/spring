14d0c08e9dc95014e0c7963ab41ed1d23091f7f9
iv0100088 <jongcheon.song@hyundai-di.com>
2022-06-09 18:10:54 +0900

    Spring MVC v1 Start

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,12 +3,22 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 
 @ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
+	/* SpringMemberFormControllerV1 @RequestMapping 만 있을 경우 
+	 * //스프링 빈 직접 등록
+	 * 
+	 * @Bean SpringMemberFormControllerV1 springMemberFormControllerV1() { return
+	 * new SpringMemberFormControllerV1(); }
+	 */
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(ServletApplication.class, args);
 	}
 

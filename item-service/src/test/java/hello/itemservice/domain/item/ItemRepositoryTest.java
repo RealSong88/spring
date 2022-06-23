27d6cca45346c89289629c404a72aac7c0ba950c
iv0100088 <jongcheon.song@hyundai-di.com>
2022-06-23 09:21:49 +0900

    스프링 MVC 7.. 33p

--- item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
+++ item-service/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -1,7 +1,6 @@
 package hello.itemservice.domain.item;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 
 import java.util.List;
 

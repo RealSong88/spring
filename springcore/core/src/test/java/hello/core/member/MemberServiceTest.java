e496302f9ee3aa4d2a9657f06222803209a5df26
iv0100088 <jongcheon.song@hyundai-di.com>
2022-07-05 09:43:21 +0900

    spring core ing...

---
+++ springcore/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,32 @@
+package hello.core.member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import hello.core.Appconfig;
+
+class MemberServiceTest {
+
+	MemberService memberService;
+
+	@BeforeEach
+	public void beforeEach() {
+		Appconfig appconfig = new Appconfig();
+		memberService = appconfig.memberService();
+	}
+
+	@Test
+	void join() {
+		//given
+		Member member = new Member(1L, "memberA", Grade.VIP);
+
+		//when
+		memberService.join(member);
+		Member findMember = memberService.findMember(1L);
+
+		//then
+		assertThat(member).isEqualTo(findMember);
+	}
+}
---
+++ springcore/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,34 @@
+package hello.core.order;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import hello.core.Appconfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+
+class OrderServiceTest {
+
+	MemberService memberService;
+	OrderService orderService;
+
+	@BeforeEach
+	public void beforeEach() {
+		Appconfig appconfig = new Appconfig();
+		memberService = appconfig.memberService();
+		orderService = appconfig.orderService();
+	}
+
+	@Test
+	void createOrder() {
+		long memberId = 1L;
+		Member member = new Member(memberId, "memberA", Grade.VIP);
+		memberService.join(member);
+
+		Order order = orderService.createOrder(memberId, "itemA", 10000);
+		assertThat(order.getDiscountPrice()).isEqualTo(1000);
+	}
+}

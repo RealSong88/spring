e18af4fb5951dd6d369e51d0df2f3656c5225e26
iv0100088 <jongcheon.song@hyundai-di.com>
2022-07-15 09:25:36 +0900

    spring core ing...4

--- springcore/core/src/main/java/hello/core/AppConfig.java
+++ springcore/core/src/main/java/hello/core/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -17,23 +17,29 @@ public class AppConfig {
 
 	@Bean
 	public MemberService memberService() {
+		//1번
+		System.out.println("call AppConfig.memberService");
 		return new MemberServiceImpl(memberRepository());
 	}
 
 	@Bean
 	public OrderService orderService() {
+		//1번
+		System.out.println("call AppConfig.orderService");
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 
 	@Bean
 	public MemberRepository memberRepository() {
+		//2번? 3번?
+		System.out.println("call AppConfig.memberRepository");
 		return new MemoryMemberRepository();
 	}
 
 	@Bean
 	public DiscountPolicy discountPolicy() {
-		return new FixDiscountPolicy();
-//		return new RateDiscountPolicy();
+//		return new FixDiscountPolicy();
+		return new RateDiscountPolicy();
 	}
 
 }
